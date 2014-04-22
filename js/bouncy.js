3fa2d0311899194e74c13d3bdd70aca4c576d4f6
Tim Alander <tim.alander@gmail.com>
2014-04-22 12:46:29 -0400

    fixed bouncing off walls at an angle

--- js/bouncy.js
+++ js/bouncy.js
@@ -7,14 +7,15 @@ function Container () {
 function Box (container, x, y) {
 	this.x = x;
 	this.y = y;
-	directions = ['up', 'down', 'left', 'right'];
-	this.direction = directions[Math.floor(Math.random()*4)];
-	console.log(this.direction)
+	this.container = container;
+	this.dx = Math.floor((Math.random()+1)*2);
+	this.dy = Math.floor((Math.random()+1)*2);
 	this.div = document.createElement("div")
 	this.div.className = "box";
 	this.div.style.left = '' + x + 'px';
 	this.div.style.top = '' + y + 'px';
 	this.div.style.backgroundColor = getRandomColor();
+	this.div.style.borderColor = getRandomColor();
 	this.div.onclick = function () {
 		box = new Box(container, Math.floor((Math.random()*container.w)), Math.floor((Math.random()*container.h)));
 		container.box_array.push(box);
@@ -22,37 +23,32 @@ function Box (container, x, y) {
 	document.getElementsByTagName('body')[0].appendChild(this.div);
 }
 
-Box.prototype.move = function () {
-
-	if (this.x > container.w) {
-		this.direction = 'left';
+Box.prototype.animate = function () {
+	window.onresize = function() {
+		this.container.w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)-50;
+		this.container.h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)-50;
 	}
-	else if (this.x < 0) {
-		this.direction = 'right';
-	}
-	else if (this.y > container.h) {
-		this.direction = 'up';
-	}
-	else if (this.y < 0) {
-		this.direction = 'down';
-	}
-
-	if (this.direction === 'up') {
-		this.y -= 1;
-	}
-	else if (this.direction === 'down') {
-		this.y += 1;
-	}
-	else if (this.direction === 'left') {
-		this.x -= 1;
-	}
-	else if (this.direction === 'right') {
-		this.x += 1;
-	}
+	this.checkBounds();
+	this.move();
 	this.div.style.left = '' + this.x + 'px';
 	this.div.style.top = '' + this.y + 'px';
 }
 
+Box.prototype.checkBounds = function () {
+	if ((this.x > this.container.w) || (this.x < 0)) {
+		this.dx = -this.dx;
+	}
+	else if ((this.y > this.container.h) || (this.y < 0)) {
+		this.dy = -this.dy;
+	}
+}
+
+Box.prototype.move = function () {
+	this.x += this.dx;
+	this.y += this.dy;
+}
+
+// from http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
 function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
     var color = '#';
@@ -63,13 +59,12 @@ function getRandomColor() {
 }
 
 window.onload = function start() {
-	container = new Container();
-
-	box = new Box(container, 4, 6); // initialize first box
+	var container = new Container();
+	var box = new Box(container, Math.floor((Math.random()*container.w)), Math.floor((Math.random()*container.h))); // initialize first box
 	container.box_array.push(box);
 	window.setInterval(function() {
 		for (var i = 0; i < container.box_array.length; i++) {
-			container.box_array[i].move();
+			container.box_array[i].animate();
 		};
 	}, 10);
 }
